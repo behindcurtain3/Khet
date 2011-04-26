37eecc251b643d13a27a5b93d94ff6d34bf564e5
behindcurtain3 <behindcurtain3@gmail.com>
2011-04-25 22:23:43 -0700

    Added PieceHelper

---
+++ src/com/behindcurtain3/khet/util/PieceHelper.java
@@ -0,0 +1,15 @@
+package com.behindcurtain3.khet.util;
+
+public class PieceHelper {
+	// Types
+	public final static int None = 0;
+	public final static int Pharaoh = 1;
+	public final static int Djed = 2;
+	public final static int Pyramid = 3;
+	public final static int SingleObelisk = 4;
+	public final static int DoubleObelisk = 5;
+	
+	// Color
+	public final static int Red = 10;
+	public final static int Silver = 11;
+}
